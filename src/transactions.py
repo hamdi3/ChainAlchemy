873fa0e81adb73e067cbcdc36a9500d4206848a4
Hamdi <Hamdi3khalil@gmail.com>
2024-09-21 11:34:00 +0200

    FIXED
    Fixed imports for PKCS1 to use it a signature instead of a cipher

--- src/transactions.py
+++ src/transactions.py
@@ -1,8 +1,8 @@
 import binascii
 from typing import OrderedDict
 from Crypto.PublicKey import RSA
-from Crypto.Cipher import PKCS1_v1_5
 from Crypto.Hash import SHA
+from Crypto.Signature import PKCS1_v1_5
 
 
 class Transaction:
